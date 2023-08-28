92fd0bc625265c649ccd15c003ced51f5e7ecd59
artem <kasatkin_16@mail.ru>
2023-08-29 02:12:01 +0400

    created model Post

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    '''Данные о посте '''
+    title = models.CharField('Заголовок поста', max_length=100)
+    descriotion = models.TextField('Текст записи')
+    author = models.CharField('Имя автора', max_length=100)
+    date = models.DateField('Дата публикации')
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+
+    class Meta:
+        verbose_name = 'Запись'
+        verbose_name_plural = 'Записи'
+
+
+
